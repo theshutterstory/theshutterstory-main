53127406969dddebd3c2c67ea68f3d1007d1aac0
shreeraj007 <shreerajmane007@gmail.com>
2024-03-27 23:35:07 +0530

    Remove : Metadata & Description

--- app/layout.js
+++ app/layout.js
@@ -1,31 +1,26 @@
-import React from "react";
-import Head from "next/head";
-import Header from "./_components/Header";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./_components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "The Shutter Story - Hire Photographers | Capture Your Memories",
-  description:
-    "The Shutter Story - Hire professional photographers for your events. Capture your memories beautifully. Book now!",
+  title: "theshutterstory",
+  description: "Hire a photographer",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <div>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content={metadata.description} />
-        <title>{metadata.title}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
-        <Header />
+        <div>
+          <Header />
+        </div>
         {children}
       </body>
-    </div>
+    </html>
   );
 }
